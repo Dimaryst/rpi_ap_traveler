e21a6f0c308f0327c08594bdbd68e10a5f2b1a83
Dimaryst <dimaryst@icloud.com>
2022-03-16 18:26:48 +0300

    basic wifi repeater settings

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, url_for, request, Response
+from werkzeug.utils import redirect
+import iwlist
+
+app = Flask(__name__, static_folder="static")
+
+
+@app.route('/wifi', methods=['GET'])
+def settings_page():
+    content = iwlist.scan(interface='wlan1')
+    iw_cells = iwlist.parse(content)
+    for cell in iw_cells:
+        if int(cell['signal_quality']) >= 66:
+            cell["signal_picture"] = "images/wifi-max.png"
+        elif 33 < int(cell['signal_quality']) < 66:
+            cell["signal_picture"] = "images/wifi-50.png"
+        else:
+            cell["signal_picture"] = "images/wifi-min.png"            
+    iwlist.remove_known_networks_from_list(iwlist.get_networks_wpa_supplicant(), iw_cells)
+    print(iwlist.get_connected_network_ssid())
+    return render_template('wifi.html', current_networks=iwlist.get_connected_network_ssid(),
+                           configured_networks=iwlist.get_networks_wpa_supplicant(),
+                           available_networks=iw_cells)
+
+
+@app.route('/wifi/password_request_<ssid>', methods=['GET', 'POST'])
+def password_request_page(ssid):
+    if request.method == 'POST':
+        password = request.form.get('password')
+        print(ssid, password)
+        iwlist.add_network_to_wpa_supplicant(ssid, password)
+        return redirect(url_for('settings_page'))
+    return render_template('request_pass.html', network_ssid=ssid)
+
+
+@app.route('/wifi/remove_network_<ssid>', methods=['GET'])
+def remove_network(ssid):
+    print(ssid)
+    iwlist.remove_network_from_wpa_supplicant(ssid)
+    return redirect(url_for('settings_page'))
+
+@app.route('/', methods=['GET'])
+def index_page():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1")
---
+++ netconfigure.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+# install mpv
+
+apt-get install mpv -y
+# deinstall classic networking
+systemctl daemon-reload
+systemctl disable --now ifupdown dhcpcd dhcpcd5 isc-dhcp-client isc-dhcp-common rsyslog
+apt --autoremove purge ifupdown dhcpcd dhcpcd5 isc-dhcp-client isc-dhcp-common rsyslog
+rm -r /etc/network /etc/dhcp
+
+# setup/enable systemd-resolved and systemd-networkd
+systemctl disable --now avahi-daemon libnss-mdns
+apt --autoremove purge avahi-daemon
+apt install libnss-resolve -y
+ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+apt-mark hold avahi-daemon dhcpcd dhcpcd5 ifupdown isc-dhcp-client isc-dhcp-common libnss-mdns openresolv raspberrypi-net-mods rsyslog
+systemctl enable systemd-networkd.service systemd-resolved.service
+
+# configure wpa_supplicant-wlan0.conf
+cat > /etc/wpa_supplicant/wpa_supplicant-wlan0.conf <<EOF
+country=RU
+ctrl_interface=DIR=/run/wpa_supplicant GROUP=netdev
+update_config=1
+p2p_disabled=1
+
+network={
+    ssid="AltairikaNet"
+    psk="qwe123qwe"
+    key_mgmt=WPA-PSK
+    mode=2
+    frequency=2412
+}
+EOF
+
+chmod 777 /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+systemctl disable wpa_supplicant.service
+systemctl enable wpa_supplicant@wlan0.service
+rfkill unblock wlan
+
+# configure wpa_supplicant for wlan1 as client
+cat > /etc/wpa_supplicant/wpa_supplicant-wlan1.conf <<EOF
+country=DE
+ctrl_interface=DIR=/run/wpa_supplicant GROUP=netdev
+update_config=1
+p2p_disabled=1
+
+
+EOF
+
+chmod 777 /etc/wpa_supplicant/wpa_supplicant-wlan1.conf
+systemctl disable wpa_supplicant.service
+systemctl enable wpa_supplicant@wlan1.service
+
+# configure interfaces
+cat > /etc/systemd/network/08-wlan0.network <<EOF
+[Match]
+Name=wlan0
+[Network]
+Address=192.168.4.1/24
+LLMNR=no
+DNSSEC=no
+MulticastDNS=yes
+# IPMasquerade is doing NAT
+IPMasquerade=yes
+IPForward=yes
+DHCPServer=yes
+[DHCPServer]
+DNS=84.200.69.80 1.1.1.1
+EOF
+
+# Because we don't have a bridge,
+# we need two different subnets. 
+# Be aware that the static ip address for the access point wlan0 belongs to another subnet than that from wlan1. 
+# For the connection to the internet router we use network address translation (NAT).
+
+cat > /etc/systemd/network/12-wlan1.network <<EOF
+[Match]
+Name=wlan1
+[Network]
+LLMNR=no
+DNSSEC=no
+MulticastDNS=yes
+DHCP=yes
+EOF
+
+# reboot
