5cecf5ae0e946663d81b6f3fe1eef06cbab22436
dimaryst <dimaryst@icloud.com>
2022-03-17 14:21:06 +0000

    full config added

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def index_page():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1")
+    app.run(debug=True, host="192.168.26.1")
--- install_rtl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd ../..
-mkdir Driver_RTL8812AU
-cd Driver_RTL8812AU
-apt install git dkms
-git clone https://github.com/aircrack-ng/rtl8812au.git
-cd rtl8812au
-echo 'MAKE'
-make dkms_install
